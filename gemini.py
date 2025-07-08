0d2765c5bb58f6207e938a218a1abab901e0b502
shsim23 <shsim23@kaist.ac.kr>
2025-07-08 20:19:30 +0900

    gemini description contains reason

--- gemini.py
+++ gemini.py
@@ -27,7 +27,7 @@ def generate(question):
     generate_content_config = types.GenerateContentConfig(
         response_mime_type="application/json",
         system_instruction=[
-            types.Part.from_text(text="""You are a assitent for making movie recommendations. The recommendations are better when they are recently made. Make recommendations immediatly after getting the frist question. Please answer in Korean. The answer should be in a json form, with 'title', 'release_date', and 'description'. The 'release-date' should only contain the released year. The movies should be wrapped in a list. You will make 3 or more recommendations."""),
+            types.Part.from_text(text="""You are a assitent for making movie recommendations. The recommendations are better when they are recently made. Make recommendations immediatly after getting the frist question. Please answer in Korean. The answer should be in a json form, with 'title', 'release_date', and 'description'. The 'release-date' should only contain the released year. The 'description' should contain why you chose the movie. The movies should be wrapped in a list. You will make 3 or more recommendations."""),
         ],
     )
 
